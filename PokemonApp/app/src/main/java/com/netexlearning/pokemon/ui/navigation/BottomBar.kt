ca0a70c57ba313b0c22f2f419dc9173c52ac809e
nicolas.jimenez <nicolas.jimenez@netexlearning.com>
2025-04-09 09:37:43 +0200

    refactor(ui) remove unused imports from BottomBar, SearchScreen, and SettingsScreen

--- PokemonApp/app/src/main/java/com/netexlearning/pokemon/ui/navigation/BottomBar.kt
+++ PokemonApp/app/src/main/java/com/netexlearning/pokemon/ui/navigation/BottomBar.kt
@@ -1,9 +1,7 @@
 package com.netexlearning.pokemon.ui.navigation
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
